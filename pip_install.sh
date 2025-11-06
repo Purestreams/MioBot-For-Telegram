94ee4e16861a0ba8972f953759fbf4ca76a0eef2
purestreams <github@amiya.eu.org>
2025-11-06 18:49:32 +0000

    update gs

--- pip_install.sh
+++ pip_install.sh
@@ -1,3 +1,3 @@
 apt-get update
-apt-get install texlive-xetex texlive-latex-extra ffmpeg imagemagick -y
+apt-get install texlive-xetex texlive-latex-extra ffmpeg imagemagick ghostscript -y
 pip install python-telegram-bot markdown2 pillow aiofiles aiohttp requests beautifulsoup4 playwright openai aiosqlite reportlab yt-dlp --upgrade

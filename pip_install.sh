1d2164c2572afe95a53b5f67f5cabfc1bff7ffd9
purestreams <github@amiya.eu.org>
2025-11-06 18:27:52 +0000

    update sh

--- pip_install.sh
+++ pip_install.sh
@@ -1,1 +1,3 @@
+apt-get update
+apt-get install texlive-xetex texlive-latex-extra
 pip install python-telegram-bot markdown2 pillow aiofiles aiohttp requests beautifulsoup4 playwright openai aiosqlite reportlab --upgrade

eeb342bdd89c41036ce89bbd8e6d429076fe08b1
purestreams <github@amiya.eu.org>
2025-12-23 08:31:36 +0000

    change to init

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 apt-get update
 apt-get install texlive-full texlive-xetex texlive-latex-extra ffmpeg -y
 pip install python-telegram-bot markdown2 pillow aiofiles aiohttp requests beautifulsoup4 playwright openai aiosqlite reportlab yt-dlp pypdfium2 --upgrade
+playwright install chromium --only-shell --with-deps

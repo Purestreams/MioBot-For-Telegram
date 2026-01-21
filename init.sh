0c20b94919667fcc91ce99c81e23f580034a39e4
purestreams <github@amiya.eu.org>
2026-01-21 08:29:19 +0000

    update DockerFile

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
-apt-get update
 apt-get install texlive-full texlive-xetex texlive-latex-extra ffmpeg -y
 python3 -m pip install --upgrade pip
 python3 -m pip install python-telegram-bot markdown2 pillow aiofiles aiohttp requests beautifulsoup4 playwright openai aiosqlite reportlab yt-dlp pypdfium2 numpy fastembed onnxruntime --upgrade

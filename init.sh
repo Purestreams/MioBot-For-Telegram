0374503b02e9ce2a4bdaa1b89fd3fe4897fd152a
purestreams <github@amiya.eu.org>
2026-03-26 08:05:10 +0000

    update init

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install texlive-latex-recommended texlive-fonts-recommended texlive-xetex texlive-latex-extra ffmpeg -y
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+uv sync
 #python3 -m pip install --upgrade pip
 #python3 -m pip install python-telegram-bot markdown2 pillow aiofiles aiohttp requests beautifulsoup4 playwright openai aiosqlite reportlab yt-dlp pypdfium2 numpy fastembed onnxruntime --upgrade
 
@@ -20,5 +21,5 @@ else
 	playwright install chromium --only-shell
 fi
 
-uv sync
+
 uv run python main.py

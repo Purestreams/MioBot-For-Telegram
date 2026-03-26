8a6dbf6c8f28e8ac09f23819c2e81bb13c21096a
purestreams <github@amiya.eu.org>
2026-03-26 08:07:40 +0000

    update init

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ apt-get update
 apt-get install texlive-latex-recommended texlive-fonts-recommended texlive-xetex texlive-latex-extra ffmpeg -y
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 uv sync
 #python3 -m pip install --upgrade pip
 #python3 -m pip install python-telegram-bot markdown2 pillow aiofiles aiohttp requests beautifulsoup4 playwright openai aiosqlite reportlab yt-dlp pypdfium2 numpy fastembed onnxruntime --upgrade
